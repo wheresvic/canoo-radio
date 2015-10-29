b07cf86ae75713b365fa0be616c184f737c23c6d
Victor Parmar <victor.parmar@canoo.com>
2015-10-29 09:19:21 +0100

    removing grunt infrastructure

--- UI/grunt/config/clean.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    index : [ '<%= config.files.app.index %>' ],
-    dev : [ '<%= config.dir.temp %>/*' ],
-    dist: [
-        '<%= config.dir.temp %>/*',
-        '<%= config.dir.dist %>/*',
-        '<%= config.dir.reports %>/*'
-    ]
-};
--- UI/src/app/dummy/dummy.js
+++
@@ -1,7 +0,0 @@
-angular.module('app.dummy', [
-    'app.dummy.controllers',
-    'app.dummy.integrationservices'
-])
-
-
-;
