02f9a7f824df4da56598717d50d7879ae6d5e540
Hannes <johannes.porzelt@canoo.com>
2015-10-29 17:43:23 +0100

    blinking play icon

--- UI-browserify/public/js/main.js
+++ UI-browserify/public/js/main.js
@@ -38,7 +38,7 @@
 
     //scroll menu
     jQuery('.appear').appear();
-    jQuery(".appear").on("appear", function(data) {
+    jQuery('.appear').on("appear", function(data) {
         var id = $(this).attr("id");
         jQuery('.nav li').removeClass('active');
         jQuery(".nav a[href='#" + id + "']").parent().addClass("active");
