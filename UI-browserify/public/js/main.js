bee0b881755763ed5e3bf2afaf1f1d751f1f330b
Hannes <johannes.porzelt@canoo.com>
2015-10-29 21:35:27 +0100

    nice colors (and there is a email validation)

--- UI-browserify/public/js/main.js
+++ UI-browserify/public/js/main.js
@@ -36,13 +36,4 @@
     }
 
 
-    //scroll menu
-    // jQuery('.appear').appear();
-    jQuery(".appear").on("appear", function(data) {
-        var id = $(this).attr("id");
-        jQuery('.nav li').removeClass('active');
-        jQuery(".nav a[href='#" + id + "']").parent().addClass("active");
-    });
-
-
 })(jQuery);
