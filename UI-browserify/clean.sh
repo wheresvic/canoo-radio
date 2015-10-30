8a68bf60a2ab90bcbac7a5fb25dd73149fbd781a
Christoph Häcki <christoph.haecki@canoo.com>
2015-10-30 18:18:48 +0100

    shell scripts update

--- UI-browserify/clean.sh
+++ UI-browserify/clean.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Remove static Content"
-if [ -d "../canooradio-server/src/main/resources/static" ]; then
-    rm -r ../canooradio-server/src/main/resources/static
+if [ -d "canooradio-server/src/main/resources/static" ]; then
+    rm -r canooradio-server/src/main/resources/static
 fi
