7726daf333e06d62a84aca9d48aaf0da0c66006e
Victor Parmar <victor.parmar@canoo.com>
2015-10-31 10:34:38 +0100

    using header

--- canooradio-ui/js/app.js
+++ canooradio-ui/js/app.js
@@ -9,6 +9,7 @@ var Chance = require('chance'),
 var app =
     angular.module('canooradio', [require('ng-file-upload')])
         .config(function($locationProvider) {
+
             // use the HTML5 History API
             $locationProvider.html5Mode({
                 enabled: false,
@@ -426,5 +427,49 @@ app.controller('RadioController',
 }]);
 
 app.run(function () {
+
+    (function($) {
+
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 100) {
+                $('.scrollup').fadeIn();
+            } else {
+                $('.scrollup').fadeOut();
+            }
+        });
+
+        $('.scrollup').click(function() {
+            $("html, body").animate({scrollTop: 0}, 1000);
+            return false;
+        });
+
+        $('#relax').click(function() {
+            $("html, body").animate({scrollTop: 0}, 1000);
+            return false;
+        });
+
+        // local scroll
+        jQuery('.navbar').localScroll({reset: true, hash: true, offset: {top: 0}, duration: 800, easing: 'easeInOutExpo'});
+
+        if (Modernizr.mq("screen and (max-width:1024px)")) {
+            jQuery("body").toggleClass("body");
+        } else {
+            var s = skrollr.init({
+                mobileDeceleration: 1,
+                edgeStrategy: 'set',
+                forceHeight: true,
+                smoothScrolling: true,
+                smoothScrollingDuration: 300,
+                easing: {
+                    WTF: Math.random,
+                    inverted: function(p) {
+                        return 1 - p;
+                    }
+                }
+            });
+        }
+
+
+    })(jQuery);
 
 });
