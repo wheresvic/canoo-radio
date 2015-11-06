689ef729ffe364ec0442a479c10489f8dbf6775b
Victor Parmar <victorparmar@gmail.com>
2015-11-06 17:24:28 +0100

    adding node mpd wrapper

---
+++ canooradio-ui/lib/mpd.js
@@ -0,0 +1,223 @@
+
+var _ = require('underscore');
+
+var mpd = require('mpd'),
+    cmd = mpd.cmd;
+
+var mpdWrapper = function (host, port, logger) {
+
+  var self = {
+
+  };
+
+  var client = mpd.connect({
+    port: 6600,
+    host: 'localhost',
+  });
+
+  client.on('ready', function() {
+
+    if (logger) {
+
+    } else {
+      console.log("mpd connected to " + host + ":" + port);
+    }
+
+  });
+
+  var getObjArrayFromMpdResponse = function (msg, key) {
+
+    var result = [];
+    var obj = {
+
+    };
+
+    var parts = msg.split('\n');
+    // console.log(parts);
+
+    _.each(parts, function (part) {
+      var kv = part.split(':');
+
+      if (kv[0] && kv[1]) {
+        var value = kv[1].replace(' ', '');
+
+        if (!isNaN(value)) {
+          value = (+value);
+        }
+
+        obj[kv[0]] = value;
+
+        if (kv[0] === key) {
+          result.push(obj);
+          obj = {
+
+          };
+        }
+      }
+
+    });
+
+    return result;
+
+  };
+
+  var getObjFromMpdResponse = function (msg) {
+
+    // console.log(msg);
+
+    var obj = {
+
+    };
+
+    var parts = msg.split('\n');
+
+    _.each(parts, function (part) {
+      var kv = part.split(':');
+
+      if (kv[0] && kv[1]) {
+        var value = kv[1].replace(' ', '');
+
+        if (!isNaN(value)) {
+          value = (+value);
+        }
+
+        obj[kv[0]] = value;
+      }
+
+    });
+
+    return obj;
+  };
+
+
+  var executeObjCmd = function (command, cb) {
+
+    client.sendCommand(cmd(command, []), function (err, msg) {
+      var obj = getObjFromMpdResponse(msg);
+      cb(err, obj);
+    });
+
+  };
+
+  self.getStatus = function (cb) {
+
+    /*
+    { volume: -1,
+    repeat: 0,
+    random: 0,
+    single: 0,
+    consume: 0,
+    playlist: 2,
+    playlistlength: 1,
+    mixrampdb: 0,
+    state: 'play',
+    song: 0,
+    songid: 1,
+    time: 5,
+    elapsed: 4.748,
+    bitrate: 192,
+    audio: 44100 }
+    */
+
+    executeObjCmd('status', cb);
+
+  };
+
+  self.getCurrentSong = function (cb) {
+
+    /*
+    { file: 'tina-malia-gayatri-mantra.mp3',
+    'Last-Modified': '2015-09-26T09',
+    Time: 457,
+    Artist: 'Tina Malia',
+    Title: 'Gayatri mantra',
+    Track: -1,
+    Date: -1,
+    Pos: 0,
+    Id: 1 }{ file: 'tina-malia-gayatri-mantra.mp3',
+    'Last-Modified': '2015-09-26T09',
+    Time: 457,
+    Artist: 'Tina Malia',
+    Title: 'Gayatri mantra',
+    Track: -1,
+    Date: -1,
+    Pos: 0,
+    Id: 1 }
+    */
+
+    executeObjCmd('currentsong', cb);
+  };
+
+  self.play = function (cb) {
+    executeObjCmd('play', cb);
+  };
+
+  self.stop = function (cb) {
+    executeObjCmd('stop', cb);
+  };
+
+  self.getCurrentPlaylistInfo = function (cb) {
+    client.sendCommand(cmd("playlistinfo", []), function (err, msg) {
+      cb(err, getObjArrayFromMpdResponse(msg, 'Id'));
+    });
+  };
+
+  self.updateDb = function (cb) {
+    executeObjCmd('update', cb);
+  };
+
+  self.addSongToPlaylist = function (filePath, cb) {
+
+    client.sendCommand(cmd("add", [filePath]), function (err, msg) {
+      cb(err, getObjFromMpdResponse(msg));
+    });
+
+  };
+
+  self.getUpcomingSongs = function (cb) {
+
+    self.getCurrentSong(function (err, song) {
+
+      if (err) {
+        cb(err);
+        return;
+      }
+
+      self.getCurrentPlaylistInfo(function (err, playlist) {
+
+        if (err) {
+          cb(err);
+          return;
+        }
+
+        if (song) {
+          cb(null, playlist.slice(song.Pos + 1));
+        } else {
+          cb(null, playlist);
+        }
+
+      });
+
+    });
+
+  };
+
+  return self;
+
+};
+
+
+/*
+client.on('system', function(name) {
+  console.log("update", name);
+});
+
+client.on('system-player', function() {
+  client.sendCommand(cmd("status", []), function(err, msg) {
+    if (err) throw err;
+    console.log(msg);
+  });
+});
+*/
+
+module.exports = mpdWrapper;
---
+++ canooradio-ui/test/test-mpd.js
@@ -0,0 +1,73 @@
+
+var expect = require("chai").expect;
+var assert = require('chai').assert;
+
+var mpdWrapper = require('../lib/mpd')('localhost', 6600);
+
+describe("mpd", function() {
+
+  it("should get mpd status", function(done) {
+
+    mpdWrapper.getStatus(function (err, status) {
+      console.log(status);
+      expect(status.volume).to.equals(-1);
+      done();
+    });
+
+  });
+
+  it("should get the current song", function(done) {
+
+    mpdWrapper.getCurrentSong(function (err, song) {
+      console.log(song);
+      done();
+    });
+
+  });
+
+  it("should update the db", function(done) {
+
+    mpdWrapper.updateDb(function (err, obj) {
+      console.log(obj);
+      done();
+    });
+
+  });
+
+  it("should get current playlist info", function(done) {
+
+    mpdWrapper.getCurrentPlaylistInfo(function (err, obj) {
+      console.log(obj);
+      done();
+    });
+
+  });
+
+  it("should get upcoming songs", function(done) {
+
+    mpdWrapper.getUpcomingSongs(function (err, obj) {
+      console.log(obj);
+      done();
+    });
+
+  });
+
+  it("should start playing music", function(done) {
+
+    mpdWrapper.play(function (err, obj) {
+      console.log(obj);
+      done();
+    });
+
+  });
+
+  it("should stop playing music", function(done) {
+
+    mpdWrapper.stop(function (err, obj) {
+      console.log(obj);
+      done();
+    });
+
+  });
+
+});
