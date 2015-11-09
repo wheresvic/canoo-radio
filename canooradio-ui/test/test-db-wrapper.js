8bb54aa089a4006bb5985da588ea7fd88dad1844
Victor Parmar <victor.parmar@canoo.com>
2015-11-09 14:54:49 +0100

    fixing db wrapper tests

--- canooradio-ui/test/test-db-wrapper.js
+++ canooradio-ui/test/test-db-wrapper.js
@@ -1,4 +1,5 @@
-
+var Chance = require('chance'),
+    chance = new Chance();
 var expect = require("chai").expect;
 var assert = require('chai').assert;
 
@@ -11,7 +12,8 @@ describe("db-wrapper", function() {
 
     it("should insert a user into the db", function(done) {
 
-      dbWrapper.addUser({name: 'snoop'}, function (err, user) {
+      dbWrapper.addUser({_id: chance.string(), name: 'snoop'}, function (err, user) {
+        expect(err).to.be.null;
         expect(user).not.to.be.null;
         done();
       });
@@ -20,8 +22,10 @@ describe("db-wrapper", function() {
 
     it("should find a user by id", function(done) {
 
-      dbWrapper.addUser({name: 'dawg'}, function (err, user) {
+      dbWrapper.addUser({_id: chance.string(), name: 'dawg'}, function (err, user) {
+        expect(err).to.be.null;
         dbWrapper.getUser(user._id, function (err, found) {
+          expect(err).to.be.null;
           expect(found).not.to.be.null;
           done();
         });
@@ -31,10 +35,16 @@ describe("db-wrapper", function() {
 
     it("should update a user", function(done) {
 
-      dbWrapper.addUser({name: 'yoyo'}, function (err, user) {
+      dbWrapper.addUser({_id: chance.string(), name: 'yoyo'}, function (err, user) {
+        expect(err).to.be.null;
+        
         user.lastname = 'ma';
+        
         dbWrapper.updateUser(user, function (err, numUpdated) {
+          expect(err).to.be.null;
+          
           dbWrapper.getUser(user._id, function (err, found) {
+            expect(err).to.be.null;
             expect(found.lastname).to.equal('ma');
             done();
           });
@@ -50,6 +60,7 @@ describe("db-wrapper", function() {
     it("should upvote a song", function(done) {
 
       dbWrapper.voteUp('migros', 'coach.mp3', function (err, num, vote) {
+        expect(err).to.be.null;
         console.log(vote); // probably undefined since this is an upsert operation
         expect(num).to.equal(1);
         done();
@@ -60,6 +71,7 @@ describe("db-wrapper", function() {
     it("should clear a vote", function(done) {
 
       dbWrapper.voteClear('random', 'random', function (err, num) {
+        if (err) throw err;
         expect(num).to.equal(0);
         done();
       });
@@ -97,6 +109,7 @@ describe("db-wrapper", function() {
         dbWrapper.voteUp('b', songId, function (err, num2) {
           expect(err).to.be.null;
           dbWrapper.getVotesForSong(songId, function (err, sum) {
+            expect(err).to.be.null;
             expect(sum).to.equal(2);
             done();
           });
